e90af91f93bb8e6df353e4d160d2e5e6fb9880d0
zoirs <zoirs@ya.ru>
2021-01-14 22:01:28 +0300

    карта в json

--- Assets/Scripts/Line/LineManager.cs
+++ Assets/Scripts/Line/LineManager.cs
@@ -25,57 +25,26 @@ namespace Line {
         }
 
         public void Start() {
-            List<Tuple<Vector3, bool>> positionsManufacture = new List<Tuple<Vector3, bool>>{
-                new Tuple<Vector3, bool>(new Vector3(2f+3.5f, 9.5f),false),        //0
-                new Tuple<Vector3, bool>(new Vector3(2f+6.5f, 11.5f),false),       //1
-                new Tuple<Vector3, bool>(new Vector3(2f+6.5f, 8.5f),false),        //2
-                new Tuple<Vector3, bool>(new Vector3(2f+9.5f, 13.5f),false),       //3
-                new Tuple<Vector3, bool>(new Vector3(2f+9.5f, 7.5f),true),//      //
-                new Tuple<Vector3, bool>(new Vector3(2f+11.5f, 11.5f),false),      //4
-                new Tuple<Vector3, bool>(new Vector3(2f+11.5f, 9.5f),false),       //5
-                new Tuple<Vector3, bool>(new Vector3(2f+14.5f, 11.5f),true),//    //
-                new Tuple<Vector3, bool>(new Vector3(2f+14.5f, 9.5f),false),       //6
-                new Tuple<Vector3, bool>(new Vector3(2f+14.5f, 7.5f),false),       //7
-
-            };
+            
+            TextAsset level = Resources.Load<TextAsset>("Levels/Level");
+            LevelDto fromJson = JsonUtility.FromJson<LevelDto>(level.text);
+            
             _manufacturers = new List<Company>();
-            foreach (Tuple<Vector3, bool> tuple in positionsManufacture) {
-                if (tuple.Item2) {
+            foreach (CompanyDto company in fromJson.companies) {
+                if (company.manufactureType == "manufacture") {
                     ManufacturerController manufacturer = _factoryManufacture.Create();
-                    manufacturer.Init(tuple.Item1.x < 14f ? ProductType.Brown : ProductType.Yellow, tuple.Item1);
+                    manufacturer.Init(company.productType, company.position);
                     _manufacturers.Add(manufacturer);                    
                 }
-                else {
-                    CityController cityController = _cityManager.Create(tuple.Item1);
-//                    int stationCount = manufacturers.Count(q => q is CityController);
-//                    cityController.Init((stationCount == 0) ? ProductType.Brown : ProductType.Yellow);
+                if (company.manufactureType == "city") {
+                    CityController cityController = _cityManager.Create(company.position);
                     _manufacturers.Add(cityController);  
                 }
             }
-
-//            ManufacturerController first = _factoryManufacture.Create();
-//            first.transform.position = new Vector3(8,8);
-//            ManufacturerController second = _factoryManufacture.Create();
-//            second.transform.position = new Vector3(14, 13);
-//            DrowLineComponent drowLineComponent = _factoryLine.Create(new List<Vector3>() {
-//                new Vector3(8, 8),
-//                new Vector3(8, 11),
-//                new Vector3(14, 13)
-//            }, new List<ManufacturerController>() {manufacturers[0], manufacturers[1]});
-
-            _factoryLine.Create(new List<Vector3>(), new List<Company>() {_manufacturers[0], _manufacturers[1]});
-            _factoryLine.Create(new List<Vector3>(), new List<Company>() {_manufacturers[0], _manufacturers[2]});
-            _factoryLine.Create(new List<Vector3>(), new List<Company>() {_manufacturers[1], _manufacturers[2]});
-            _factoryLine.Create(new List<Vector3>(), new List<Company>() {_manufacturers[1], _manufacturers[3]});
-            _factoryLine.Create(new List<Vector3>(), new List<Company>() {_manufacturers[2], _manufacturers[4]});
-            _factoryLine.Create(new List<Vector3>(), new List<Company>() {_manufacturers[3], _manufacturers[5]});
-            _factoryLine.Create(new List<Vector3>(), new List<Company>() {_manufacturers[4], _manufacturers[6]});
-            _factoryLine.Create(new List<Vector3>(), new List<Company>() {_manufacturers[5], _manufacturers[7]});
-            _factoryLine.Create(new List<Vector3>(), new List<Company>() {_manufacturers[6], _manufacturers[8]});
-            _factoryLine.Create(new List<Vector3>(), new List<Company>() {_manufacturers[7], _manufacturers[8]});
-            _factoryLine.Create(new List<Vector3>(), new List<Company>() {_manufacturers[8], _manufacturers[9]});
-            _factoryLine.Create(new List<Vector3>(), new List<Company>() {_manufacturers[5], _manufacturers[6]});
-//            _lines.Add(LineType.NOT_USED, drowLineComponent.gameObject.GetComponent<LineController>());
+            foreach (LineDto line in fromJson.lines) {
+                Debug.Log("create line");
+                _factoryLine.Create(new List<Vector3>(), new List<Company>() {_manufacturers[line.indexFirst], _manufacturers[line.indexSecond]});
+            }
         }
 
         public void Create(LineType lineType) {
